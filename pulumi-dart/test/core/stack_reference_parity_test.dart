fad1b4595c256e61067262b7ff32f3f0365af8bb
kingwill101 <hey@glenfordwilliams.com>
2026-02-27 08:44:34 -0500

    expand stack reference parity tests for fallback and args mapping

--- pulumi-dart/test/core/stack_reference_parity_test.dart
+++ pulumi-dart/test/core/stack_reference_parity_test.dart
@@ -36,9 +36,11 @@ Value _toValue(dynamic value) {
 void main() {
   group('stack reference parity', () {
     late MockDeploymentImpl mockDeployment;
+    late Inputs? capturedArgs;
 
     setUp(() {
       mockDeployment = MockDeploymentImpl();
+      capturedArgs = null;
 
       when(mockDeployment.stack).thenThrow(StateError('Stack not set'));
       when(mockDeployment.registerResourceOperation(any)).thenAnswer((_) {});
@@ -52,6 +54,7 @@ void main() {
           registerPackageRequest: anyNamed('registerPackageRequest'),
         ),
       ).thenAnswer((invocation) async {
+        capturedArgs = invocation.namedArguments[#args] as Inputs;
         final resource = invocation.namedArguments[#resource] as Resource;
         resource.resolveUrn(
           'urn:pulumi:stack::project::${resource.getResourceType()}::${resource.getResourceName()}',
@@ -125,6 +128,14 @@ void main() {
       },
     );
 
+    test('getOutputDetails returns plain value for non-secret outputs', () async {
+      final reference = StackReference('ref');
+      final details = await reference.getOutputDetails('normal');
+
+      expect(details.value, equals('plain'));
+      expect(details.secretValue, isNull);
+    });
+
     test('getValue and requireValue reject secret outputs', () async {
       final reference = StackReference('ref');
 
@@ -136,6 +147,82 @@ void main() {
         reference.requireValue(Input.fromValue('secret')),
         throwsA(isA<Exception>()),
       );
+    });
+
+    test('getValue and requireValue return non-secret outputs', () async {
+      final reference = StackReference('ref');
+
+      expect(await reference.getValue(Input.fromValue('normal')), equals('plain'));
+      expect(
+        await reference.requireValue(Input.fromValue('normal')),
+        equals('plain'),
+      );
+    });
+
+    test('unknown output name yields unknown output data', () async {
+      final reference = StackReference('ref');
+      final unknownName = Input.fromOutput(Output.createUnknown<String>());
+
+      final data = await reference.getOutput(unknownName).getData();
+
+      expect(data.isKnown, isFalse);
+      expect(data.value, isNull);
+      expect(data.isSecret, isTrue);
+    });
+
+    test(
+      'secret-name fallback uses outputs secret bit when secretOutputNames is null',
+      () async {
+        when(
+          mockDeployment.readOrRegisterResource(
+            resource: anyNamed('resource'),
+            remote: anyNamed('remote'),
+            newDependency: anyNamed('newDependency'),
+            args: anyNamed('args'),
+            opts: anyNamed('opts'),
+            registerPackageRequest: anyNamed('registerPackageRequest'),
+          ),
+        ).thenAnswer((invocation) async {
+          final resource = invocation.namedArguments[#resource] as Resource;
+          resource.resolveUrn(
+            'urn:pulumi:stack::project::${resource.getResourceType()}::${resource.getResourceName()}',
+          );
+
+          final outputsValue = Value()
+            ..structValue = (Struct()
+              ..fields['normal'] = _toValue('plain')
+              ..fields['secret'] = _toValue('hidden'));
+          final secretWrappedOutputs = Struct()
+            ..fields[Constants.specialSigKey] = (Value()
+              ..stringValue = Constants.specialSecretSig)
+            ..fields[Constants.valueName] = outputsValue;
+
+          final responseObject = Struct()
+            ..fields['name'] = (Value()..stringValue = 'org/project/dev')
+            ..fields['outputs'] = (Value()..structValue = secretWrappedOutputs);
+          resource.resolveOutputs(responseObject);
+        });
+
+        final reference = StackReference('ref');
+        final normal = await reference
+            .getOutput(Input.fromValue('normal'))
+            .getData();
+
+        expect(normal.value, equals('plain'));
+        expect(normal.isSecret, isTrue);
+      },
+    );
+
+    test('StackReferenceArgs.name overrides constructor name mapping', () async {
+      final reference = StackReference(
+        'ref',
+        args: StackReferenceArgs(name: Input.fromValue('custom/stack/name')),
+      );
+      await reference.name.getValue();
+
+      expect(capturedArgs, isNotNull);
+      final mappedName = capturedArgs!['name'] as Input<String>;
+      expect(await mappedName.toOutput().getValue(), equals('custom/stack/name'));
     });
   });
 }
