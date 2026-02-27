ba88ba4f9f32dc15a09939a379e3c58798a2ad12
kingwill101 <hey@glenfordwilliams.com>
2026-02-27 09:31:42 -0500

    expand resource constructor parity tests for transform and parent merge

--- pulumi-dart/test/core/resource_constructor_parity_test.dart
+++ pulumi-dart/test/core/resource_constructor_parity_test.dart
@@ -17,6 +17,10 @@ class _BadComponent extends ComponentResource {
     : super('my:module:BadComponent', name, arg as dynamic, null);
 }
 
+class _TestStack extends Stack {
+  _TestStack();
+}
+
 void main() {
   group('resource constructor parity', () {
     late MockDeploymentImpl mockDeployment;
@@ -61,6 +65,100 @@ void main() {
 
     test('component rejects non-mapping args', () {
       expect(() => _BadComponent('bad', 4), throwsA(isA<TypeError>()));
+    });
+
+    test(
+      'resource transformations can rewrite args/options with stable parent',
+      () {
+        final root = DependencyResource(
+          'urn:pulumi:stack::project::pkg:type::root',
+        );
+        final transformedOptions = ComponentResourceOptions(
+          parent: root,
+          protect: true,
+        );
+
+        _ValidComponent(
+          'rewritten',
+          {'value': 1},
+          ComponentResourceOptions(
+            parent: root,
+            resourceTransformations: [
+              (args) => ResourceTransformationResult(
+                Input.mapToInputs({'value': 42}),
+                transformedOptions,
+              ),
+            ],
+          ),
+        );
+
+        final captured = verify(
+          mockDeployment.readOrRegisterResource(
+            resource: anyNamed('resource'),
+            remote: anyNamed('remote'),
+            newDependency: anyNamed('newDependency'),
+            args: captureAnyNamed('args'),
+            opts: captureAnyNamed('opts'),
+            registerPackageRequest: anyNamed('registerPackageRequest'),
+          ),
+        ).captured;
+
+        final capturedArgs = captured[0] as Map<String, Input<dynamic>>;
+        final capturedOpts = captured[1] as ResourceOptions;
+        expect(capturedArgs['value'], isNotNull);
+        expect(capturedOpts.protect, isTrue);
+        expect(identical(capturedOpts.parent, root), isTrue);
+      },
+    );
+
+    test('resource transformations cannot change parent', () {
+      final root = DependencyResource(
+        'urn:pulumi:stack::project::pkg:type::root',
+      );
+      final other = DependencyResource(
+        'urn:pulumi:stack::project::pkg:type::other',
+      );
+
+      expect(
+        () => _ValidComponent(
+          'invalid-parent-transform',
+          {'value': 1},
+          ComponentResourceOptions(
+            parent: root,
+            resourceTransformations: [
+              (args) => ResourceTransformationResult(
+                Input.mapToInputs({'value': 2}),
+                ComponentResourceOptions(parent: other),
+              ),
+            ],
+          ),
+        ),
+        throwsA(isA<ArgumentError>()),
+      );
+    });
+
+    test('resource constructor merges inferred stack parent when unset', () {
+      final inferredParent = _TestStack();
+      when(mockDeployment.stack).thenReturn(inferredParent);
+      clearInteractions(mockDeployment);
+
+      _ValidComponent('inherits-parent', {
+        'value': 1,
+      }, ComponentResourceOptions());
+
+      final captured = verify(
+        mockDeployment.readOrRegisterResource(
+          resource: anyNamed('resource'),
+          remote: anyNamed('remote'),
+          newDependency: anyNamed('newDependency'),
+          args: anyNamed('args'),
+          opts: captureAnyNamed('opts'),
+          registerPackageRequest: anyNamed('registerPackageRequest'),
+        ),
+      ).captured;
+
+      final capturedOpts = captured.single as ResourceOptions;
+      expect(identical(capturedOpts.parent, inferredParent), isTrue);
     });
   });
 }
