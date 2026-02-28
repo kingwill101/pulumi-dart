66d651882c6a5180cd415a7ae87416526eaf260a
kingwill101 <hey@glenfordwilliams.com>
2026-02-28 12:40:29 -0500

    chore: remove stale upstream dotnet sdk directory

--- sdk/Pulumi.Automation/AssemblyAttribute.cs
+++
@@ -1,5 +0,0 @@
-// Copyright 2016-2021, Pulumi Corporation
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Pulumi.Automation.Tests")]
--- sdk/Pulumi/AssemblyAttributes.cs
+++
@@ -1,5 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Pulumi.Automation")]
-[assembly: InternalsVisibleTo("Pulumi.Tests")]
-[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")] // Moq tests
